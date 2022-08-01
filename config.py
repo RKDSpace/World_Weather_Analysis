ce94f4e930b8826776b3ada97a5b4e046106b256
RKDSpace <reshpal01@hotmail.com>
2022-07-31 20:52:59 -0700

    challenge

--- config.py
+++
@@ -1,8 +0,0 @@
-# NYT key
-NYT_KEY = "eoBbqlrhB2GCI9cSZIAPo8hRa4VzMqVB"
-
-# GMap key
-g_key = "AIzaSyBbLes_fzGaO1M7frV0pYoI-eOgrh7-ibo"
-
-#openweather key
-open_weather = "68aafe498c27cc0370d51ba409ab55b0"
